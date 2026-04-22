92ff0035a837e1302341cbec943daef74af13fee
Zulfani Z <kajubbaju@gmail.com>
2026-04-22 22:19:11 +0700

    pengerjaan geometry

---
+++ bangun_ruang/balok.js
@@ -0,0 +1,15 @@
+const prompt = require("prompt-sync")({ sigint: true });
+let p = Number(prompt("Masukkan panjang : "));
+let l = Number(prompt("Masukkan lebar : "));
+let t = Number(prompt("Masukkan tinggi : "));
+
+let volume = p * l * t;
+let luas = 2 * (p * l + p * t + l * t);
+
+console.log("===========================")
+console.log("         H A S I L         ")
+console.log("===========================")
+
+console.log(
+`Volume Balok\t\t: ${volume.toFixed(2)}  cm2
+Luas Permukaan Balok\t: ${luas.toFixed(2)}  cm`)
