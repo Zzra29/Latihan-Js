8a3a4c903389694842ba3db7229440fbd81ffa5a
Zulfani Z <kajubbaju@gmail.com>
2026-04-22 22:27:08 +0700

    pengerjaan geometry

--- bangun_datar/segitiga.js
+++ bangun_datar/segitiga.js
@@ -4,7 +4,7 @@ let b = Number(prompt("Masukkan sisi miring 1 : "));
 let c = Number(prompt("Masukkan sisi miring 2 : "));
 let tinggi = Number(prompt("Masukkan tinggi : "));
 
-let luas = a * tinggi;
+let luas = 0.5 * a * tinggi;
 let keliling = a + b + c;
 
 console.log("===========================")
