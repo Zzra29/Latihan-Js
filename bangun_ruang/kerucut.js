8d7f794308fbda36cd2267e5b6c4ec3fbdb44c77
Zulfani Z <kajubbaju@gmail.com>
2026-04-23 08:02:06 +0700

    pengerjaan

--- bangun_ruang/kerucut.js
+++ bangun_ruang/kerucut.js
@@ -1,5 +1,5 @@
 const prompt = require("prompt-sync")({ sigint: true });
-let r = Number(prompt("Masukkan jari-jaroo : "));
+let r = Number(prompt("Masukkan jari-jari : "));
 let tinggi = Number(prompt("Masukkan tinggi : "));
 
 let volume = (1/3) * 3.14 * r * r * tinggi;
